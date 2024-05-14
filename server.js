d040262acefca75117c396580d9dc2d1ccb11546
bowersjuan <bowersjuan@gmail.com>
2024-05-14 07:29:40 -0400

    Initialize back-end app, install express, morgan, dotenv and set up gitignore and env files

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8888;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
