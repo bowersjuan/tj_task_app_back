e841911a643fc733d20aacb2776eab0e3f36ae6d
bowersjuan <bowersjuan@gmail.com>
2024-05-23 15:14:19 -0400

    Modify users.sql with more properties and add reference to roles

--- src/db/users.sql
+++ src/db/users.sql
@@ -1,7 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (  
   id SERIAL PRIMARY KEY,   
-  username VARCHAR(255) NOT NULL UNIQUE,   
+  employee_number INTEGER NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,   
-  email VARCHAR(255) NOT NULL UNIQUE,   
+  email VARCHAR(255) NOT NULL UNIQUE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  employment_status VARCHAR(255) NOT NULL,
+  role_id INT REFERENCES roles(id),
+  clocked_in BOOLEAN DEFAULT FALSE,
   created_at TIMESTAMP DEFAULT NOW(),   
-  updated_at TIMESTAMP DEFAULT NOW());
+  updated_at TIMESTAMP DEFAULT NOW()
+);
