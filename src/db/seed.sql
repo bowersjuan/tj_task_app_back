195d66266c627c6ab54a5a7a8eb9729bd8d966a5
bowersjuan <bowersjuan@gmail.com>
2024-05-24 02:02:28 -0400

    Create seed.sql

---
+++ src/db/seed.sql
@@ -0,0 +1,29 @@
+\c tj_task_app_database;
+
+INSERT INTO tasks (
+  task_name
+) VALUES 
+('Register'),
+('Product Team'),
+('Bagging'),
+('Helms'),
+('Lunch')
+
+INSERT INTO users (
+  employee_number, 
+  password, 
+  email, 
+  first_name, 
+  last_name, 
+  employment_status, 
+  employee_rank, 
+  clocked_in
+) VALUES (
+  1280890, 
+  'abc123',
+  'bowersjuan@gmail.com',
+  'Juan',
+  'Bowers',
+  'Active',
+  'Crew'
+);
